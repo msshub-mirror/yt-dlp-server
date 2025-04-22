b8d97b9f185e450e8ea13c5f9e3d1780781e162c
msshub-mirror <zakuro877.5@gmail.com>
2025-04-22 11:39:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.post('/download', (req, res) => {
     return res.status(400).json({ error: 'URL is required' });
   }
 
-  const cmd = `./yt-dlp -v -f bestvideo+bestaudio --merge-output-format mp4 -o videos/sample.mp4 ${url}`;
+  const cmd = `./yt-dlp -v --cookies-from-browser chrome -f bestvideo+bestaudio --merge-output-format mp4 -o videos/sample.mp4 ${url}`;
 
 
   exec(cmd, (err, stdout, stderr) => {
